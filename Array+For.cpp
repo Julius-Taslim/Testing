01bacc28a31e90e69b32741d2db653a60bfb4bf7
Julius-Taslim <113356526+Julius-Taslim@users.noreply.github.com>
2022-11-08 20:35:09 +0700

    Add files via upload

---
+++ Array+For.cpp
@@ -0,0 +1,36 @@
+//array + for
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nilai[30];
+    string nama[30];
+    int jumlah,a;
+    char konfirmasi;
+
+    cout <<"Masukkan jumlah data siswa yang akan diinput : ";
+    cin >>jumlah;
+
+    for (a = 0; a < jumlah;a++){
+        cout <<"Masukkan Data anak ke-"<<a+1<<" = "<<endl;
+        cout <<"Masukkan Nama : ";
+        cin >> nama[a];
+        cout <<"Masukkan Nilai : ";
+        cin >> nilai[a];
+        cout <<endl;
+    }
+
+    cout <<"\nIngin Melihat Data siswa (Y/T)? : ";
+    cin >>konfirmasi;
+
+    if (konfirmasi == 'T'||konfirmasi == 't'){
+        return 0;
+    } else {
+    for (a = 0; a < jumlah;a++){
+        cout <<"\nNilai dari "<<nilai[a]<<" adalah = "<<nilai[a]<<endl;
+    }
+    }
+
+
+}
